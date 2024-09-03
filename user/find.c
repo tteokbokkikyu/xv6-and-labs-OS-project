a890c7c92b85e4a1dad357eaf88f7e13ed5f5f15
shiningxyone <2249027756@qq.com>
2024-09-03 17:23:37 +0800

    update

--- user/find.c
+++ user/find.c
@@ -2,65 +2,37 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 #include "kernel/fs.h"
- 
-void find(char *path, char *target)
-{
-        char buf[512], *p;
-        int fd;
-        struct dirent de;
-        struct stat st;
- 
-        if((fd = open(path, 0)) < 0){
-                fprintf(2, "find: cannot open %s\n", path);
-                return;
+
+void find(char* path, char* filename) {
+	char buf[512], * p;
+	int fd;
+	struct dirent de;
+	struct stat st;
+	strcpy(buf, path);
+	p = buf + strlen(buf);
+	*p++ = '/';
+	fd=open(path,0);
+    while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+        if (de.inum == 0)
+            continue;
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if (stat(buf, &st) < 0) {
+            printf("ls: cannot stat %s\n", buf);
+            continue;
         }
-        if(fstat(fd, &st) < 0){
-                fprintf(2, "find: cannot stat %s\n", path);
-                close(fd);
-                return;
+        if (st.type == T_DIR && strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
+            find(buf, filename);
         }
-        switch(st.type)
-        {
-            case T_FILE:
-                        if(strcmp(path + strlen(path) - strlen(target), target) == 0){
-                                printf("%s\n", path);
-                        }
-                        break;
-                case T_DIR:
-                        if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-                                printf("find: path too long\n");
-                                break;
-                        }
-                        strcpy(buf, path);
-                        p = buf + strlen(buf);
-                        *p++ = '/';
-                        while(read(fd, &de, sizeof(de)) == sizeof(de)){
-                                if(de.inum == 0)
-                                        continue;
-                                memmove(p, de.name, DIRSIZ);
-                                p[DIRSIZ] = 0;
-                                if(stat(buf, &st) < 0){
-                                        printf("find: cannot stat %s\n", buf);
-                                        continue;
-                                }
-                                if(strcmp(".", de.name) != 0 && strcmp("..", de.name) != 0){
-                                        find(buf, target);
-                                }
-                        }
-                        break;
-                }
-        close(fd);
+        else if (strcmp(filename, p) == 0)
+            printf("%s\n", buf);
+    }
 }
- 
-int main(int argc, char *argv[]){
-        if(argc != 3)
-        {
-                printf("input arguments: find <path> <filename>\n");
-                exit(1);
-        }
-        find(argv[1], argv[2]);
-        exit(0);
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("error\n usage:find <path><filename>\n ");
+        exit(1);
+    }
+    find(argv[1], argv[2]);
+    exit(0);
 }
- 
-          
- 
